a21344f5f3b96b6618bead05ffe369da4bb56106
Sungwon Lyu <lyusungwon@naver.com>
2015-09-14 04:30:24 +0000

    organizing

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150914035455) do
+ActiveRecord::Schema.define(version: 20150914042448) do
+
+  create_table "evals", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "hairshops", force: :cascade do |t|
     t.datetime "created_at", null: false
